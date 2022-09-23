1fab2ead379a80a47f757c8b331efcae54ab7353
Jasper Kent <software@jasperkent.com>
2022-09-23 08:55:52 +0100

    Add project files.

---
+++ Parallelization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+
+var usage = new ConcurrentDictionary<int, int>();
+var resultsBag = new ConcurrentBag<double>();
+
+var values = Enumerable.Range(1, 10);
+
+var parallel = values.AsParallel().AsOrdered();
+
+var sum = (from val in parallel 
+               select Flag(val * val)).AsSequential().Aggregate((sum, next)=>
+               {
+                   Console.WriteLine($"{sum} - {next} on {Environment.CurrentManagedThreadId}");
+                   
+                   return 2 * sum + next;
+               });
+
+Console.WriteLine(sum);
+
+//sequence.ForAll(val =>
+//{
+//    Console.WriteLine($"Displaying {val} on {Environment.CurrentManagedThreadId}");
+//});
+
+//foreach (var i in sequence)
+//    Console.WriteLine(i);
+
+int Flag(int n)
+{
+    Console.WriteLine($"Processing {n} on {Environment.CurrentManagedThreadId}");
+
+    return n;
+}
